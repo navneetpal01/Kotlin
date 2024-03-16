0adf3712db80ff2bba3e079fd7f0f9c6a7ece103
navneetpal01 <navneetpalgithub@gmail.com>
2024-03-16 16:15:50 +0530

    Week 3

--- app/src/main/java/com/week3/OOP.kt
+++ app/src/main/java/com/week3/OOP.kt
@@ -42,7 +42,7 @@ class PbJar {
 }
 
 
-fun main() {
+fun main1() {
     //Encapsulation -  great for control
     val jar = PbJar()  //Creates instance of PbJar
     println(jar.amountOfPb)
@@ -58,3 +58,89 @@ fun main() {
     println(jar.amountOfPb)
 
 }
+
+
+//Inheritance
+//Kingdom Phylum (or Division) Class Order Family Genus Species
+
+//Kotlin design flow - everything is final/closed by default
+open class Animal {   //should be abstract - open makes it extendable
+    var name: String = "Batman"
+    fun run() {
+        println("Running..........")
+    }
+
+    open fun bloodTempRange(): IntRange {
+        return 0..100
+    }
+}
+
+//when something is abstract it had to be open
+// abstract - cannot create it - "concept"
+
+abstract class Mammal : Animal() {
+
+    override fun bloodTempRange(): IntRange {  //Polymorphism - override subclass function/properties
+        return 80..100
+    }
+
+    //override and overload
+    //  -overload different signatures with same name - only parameters
+    //          -reducing namespace/surface area of the type
+    //  -override same name and parameters
+    //          -changing behaviour
+
+
+
+
+
+
+}
+
+class Cat : Mammal()
+
+class Dog : Mammal()
+
+
+fun main() {
+    val animal: Animal = Cat()
+    println(animal.name)
+    animal.run()
+    val cat: Cat = Cat()
+    println(cat.name)
+    cat.run()
+
+    goAnimal(animal = Cat())
+    goAnimal(animal = Dog())
+//  goAnimal(animal = Mammal()) can't create abstract!!
+}
+
+
+fun goAnimal(animal: Animal) {
+    println(animal.name)
+    animal.run()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
